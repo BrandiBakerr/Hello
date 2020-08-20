feba616945ccf0f162d5dc3c8d94b6efdb38019d
BrandiBakerr <69980930+BrandiBakerr@users.noreply.github.com>
2020-08-20 12:25:22 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*****
+  Hello
+  Brandi Baker
+  8/20/2020
+  CMSC225 section 001
+
+*****/
+
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Brandi Baker");
+		System.out.println("lab 1");
+		System.out.println("students are downloading notepad++ and producing their first code");
+		System.out.println("8/20/20202");
+		System.out.println("CMSC225 section 001");
+		System.out.println();
+	}
+
+}
